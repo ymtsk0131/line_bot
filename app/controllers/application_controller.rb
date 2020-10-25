e1374692e2331adf2fa3f0a1e463834962d61e83
ymtsk0131 <yuki.mtsk0131+github@gmail.com>
2020-10-26 01:38:01 +0900

    Setup line bot

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def line_client
+    @line_client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    end
+  end
+
+  def slack_client
+    @slack_client ||= Slack::Web::Client.new
+  end
 end
---
+++ app/controllers/webhook_controller.rb
@@ -0,0 +1,37 @@
+class WebhookController < ApplicationController
+  CHANNEL = '#line_bot'
+
+  def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless line_client.validate_signature(body, signature)
+      error 400, 'Bad Request'
+    end
+
+    events = line_client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          post_message(event.message['text'])
+        when Line::Bot::Event::MessageType::Image
+          post_message('写真が送信されました。')
+        when Line::Bot::Event::MessageType::Video
+          post_message('ビデオが送信されました。')
+        when Line::Bot::Event::MessageType::Sticker
+          post_message('スタンプが送信されました。')
+        end
+      end
+    end
+
+    head :ok
+  end
+
+  private
+
+  def post_message(text)
+    slack_client.chat_postMessage(channel: CHANNEL, text: text)
+  end
+end
